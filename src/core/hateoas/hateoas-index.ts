62575a09394535d8b03644bdea9631dd2057b9c6
rogerfernandez23 <rogerio.fernandes2212@gmail.com>
2024-08-23 20:31:16 -0300

    chore: updating hateoas

--- src/core/hateoas/hateoas-index.ts
+++ src/core/hateoas/hateoas-index.ts
@@ -3,6 +3,8 @@ import { HateoasData } from './hateoas-data';
 import { HateoasLinks } from './hateoas-interface';
 import { UsersController } from 'src/modules/users/users.controller';
 import { AddressController } from 'src/modules/address/address.controller';
+import { MeController } from 'src/modules/me/me.controller';
+import { AuthController } from 'src/modules/auth/auth.controller';
 
 @Injectable()
 export class HateoasIndex extends HateoasData {
@@ -18,9 +20,86 @@ export class HateoasIndex extends HateoasData {
 
     this.addLinks(
       'GET',
+      'find_user_by_id',
+      UsersController,
+      UsersController.prototype.findOne,
+    );
+
+    this.addLinks(
+      'POST',
+      'create_user',
+      UsersController,
+      UsersController.prototype.create,
+    );
+
+    this.addLinks(
+      'DELETE',
+      'delete_user',
+      UsersController,
+      UsersController.prototype.remove,
+    );
+
+    this.addLinks(
+      'GET',
+      'find_user_by_id',
+      UsersController,
+      UsersController.prototype.findOne,
+    );
+
+    this.addLinks(
+      'POST',
+      'login_user',
+      AuthController,
+      AuthController.prototype.auth,
+    );
+
+    this.addLinks(
+      'POST',
+      'logout_user',
+      AuthController,
+      AuthController.prototype.logout,
+    );
+
+    this.addLinks(
+      'POST',
+      'refresh_token',
+      AuthController,
+      AuthController.prototype.newAuthenticate,
+    );
+
+    this.addLinks(
+      'GET',
+      'user_logged_in',
+      MeController,
+      MeController.prototype.me,
+    );
+
+    this.addLinks(
+      'GET',
       'return_all_address',
       AddressController,
       AddressController.prototype.findAll,
+    );
+
+    this.addLinks(
+      'GET',
+      'validate_cep',
+      AddressController,
+      AddressController.prototype.validateCep,
+    );
+
+    this.addLinks(
+      'POST',
+      'create_address',
+      AddressController,
+      AddressController.prototype.create,
+    );
+
+    this.addLinks(
+      'PATCH',
+      'update_address',
+      AddressController,
+      AddressController.prototype.update,
     );
 
     return this.LINKS;
